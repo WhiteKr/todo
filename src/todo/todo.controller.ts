41ffdc6643eccf5b74ac30f8ee6583def3184627
WhiteKr <hanjisu2215g@gmail.com>
2024-02-24 04:16:18 +0900

    docs: configure api tags and summaries

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { Todo } from '../todo.interface';
 import { AddTodoDto } from './dto/addTodo.dto';
 import { GetTodoDto } from './dto/getTodo.dto';
@@ -8,11 +9,19 @@ import { TodoService } from './todo.service';
 export class TodoController {
   constructor(private readonly appService: TodoService) {}
 
+  @ApiOperation({
+    tags: ['todo'],
+    summary: 'Get a TODO with {id}',
+  })
   @Get(':id')
   getTodo(@Param() getTodoDto: GetTodoDto): Todo {
     return this.appService.getTodo(getTodoDto);
   }
 
+  @ApiOperation({
+    tags: ['todo'],
+    summary: 'Add a new TODO',
+  })
   @Post()
   addTodo(@Body() addTodoDto: AddTodoDto): Todo {
     return this.appService.addTodo(addTodoDto);
